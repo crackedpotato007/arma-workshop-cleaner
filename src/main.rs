37914292a0250b55ba885599e1df59f7011e5338
crackedpotato007 <arnavdrsk7633@gmail.com>
2024-10-17 22:36:31 +0530

    Initial, create logic to extract subscribed mod ids

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use csv::Reader;
+fn main() {
+    let mut csv = Reader::from_path("./files.csv").expect("Failed to open CSV file");
+    let records = csv.records();
+    //place holder path
+    let steam_path = "C:\\Program Files x86\\Steam\\steamapps\\common\\workshop\\content\\107410";
+    for record in records {
+        let string = record.expect("Something went wrong");
+        let url = string.get(1).expect("Unable to extract URL!");
+        let url_vec: Vec<&str> = url.split("=").collect();
+        let id = url_vec.get(1).expect("UNable to extract ID!");
+        println!("{:?}", id)
+    }
+}
