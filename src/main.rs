714a2a3c0dda588a13702ab105559a6a64cab715
crackedpotato007 <arnavdrsk7633@gmail.com>
2024-10-17 22:36:36 +0530

    Update placeholder path

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ fn main() {
     let mut csv = Reader::from_path("./files.csv").expect("Failed to open CSV file");
     let records = csv.records();
     //place holder path
-    let steam_path = "C:\\Program Files x86\\Steam\\steamapps\\common\\workshop\\content\\107410";
+    let _steam_path = "C:\\Program Files x86\\Steam\\steamapps\\common\\workshop\\content\\107410";
     for record in records {
         let string = record.expect("Something went wrong");
         let url = string.get(1).expect("Unable to extract URL!");
